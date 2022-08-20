04b3e3d26b1d9d5079a206ff3d6b4fa3bc1ef18d
Brian Coombs <bjcoombs1@gmail.com>
2022-08-19 23:48:59 -0500

    last commit for the night

--- shadow_dog_animation/script.js
+++ shadow_dog_animation/script.js
@@ -1,3 +1,5 @@
+//course: https://www.youtube.com/watch?v=GFO_txvwK_c&t=138s
+
 
 const canvas = document.getElementById('canvas1');
 
@@ -11,6 +13,69 @@ const playerImage = new Image();
 playerImage.src = './resources/shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
+let frameX = 0;
+let frameY = 8;
+let gameFrame = 0;
+const staggerFrames = 4;
+let animations = [
+  {
+    name: "idle",
+    frames: 7,
+  },
+  {
+    name: "jump",
+    frames: 7,
+  },
+  {
+    name: "fall",
+    frames: 9,
+  },
+  {
+    name: "run",
+    frames: 7,
+  },
+  {
+    name: "dizzy",
+    frames: 11,
+  },
+  {
+    name: "dizzy",
+    frames: 11,
+  },
+  {
+    name: "sit",
+    frames: 5,
+  },
+  {
+    name: "roll",
+    frames: 7,
+  },
+  {
+    name: "bite",
+    frames: 7,
+  },
+  {
+    name: "ko",
+    frames: 12,
+  },
+  {
+    name: "getHit",
+    frames: 4,
+  },
+];
+
+let numAnimations = {
+  0: 6,
+  1: 6,
+  2: 6,
+  3: 8,
+  4: 10,
+  5: 4,
+  6: 6,
+  7: 6,
+  8: 11,
+  9: 3,
+}
 
 function animate(){
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -18,8 +83,17 @@ function animate(){
   //ctx.fillRect(50,50,100,100);
   //To draw a whole image
   //ctx.drawImage(playerImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.drawImage(playerImage, 0,2*spriteHeight,spriteWidth,spriteHeight,0,0,spriteWidth,spriteHeight);
+
+  //Moves through animations horizontally
+
+  //Cycles through animations
+  ctx.drawImage(playerImage, frameX*spriteWidth,frameY*spriteHeight,spriteWidth,spriteHeight,0,0,spriteWidth,spriteHeight);
+  if(gameFrame % staggerFrames == 0){
+    if(frameX < animations[frameY].frames) frameX++;
+    else frameX=0;
+  }
+  gameFrame++
+
   requestAnimationFrame(animate);
 }
-
 animate();
